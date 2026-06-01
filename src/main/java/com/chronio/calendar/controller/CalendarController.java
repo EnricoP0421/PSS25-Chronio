b205689a9caec79fed5b74d70c936019a2996abb
Aurora Loprino <aurora.loprino@studio.unibo.it>
2026-06-01 13:12:39 +0200

    add tag methods to CalendarController

--- src/main/java/com/chronio/calendar/controller/CalendarController.java
+++ src/main/java/com/chronio/calendar/controller/CalendarController.java
@@ -1,12 +1,23 @@
 package com.chronio.calendar.controller;
 
 import com.chronio.calendar.model.Event;
+import com.chronio.calendar.model.Tag;
 
 import java.util.LinkedHashMap;
 import java.util.LinkedList;
 import java.util.Optional;
 
 public interface CalendarController {
+
+    LinkedHashMap<String, Tag> getTags();
+
+    Tag createTag(String name, String color);
+
+    Optional<Tag> updateTag(String id, String name, String color);
+
+    void toggleTagVisibility(String id);
+
+    void deleteTag(String id);
 
     LinkedHashMap<String, Event> getEvents();
 
