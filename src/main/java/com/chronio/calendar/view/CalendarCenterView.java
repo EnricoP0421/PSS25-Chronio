8edf1a35bb796a3dfebf37660b859069e7410ec9
Aurora Loprino <aurora.loprino@studio.unibo.it>
2026-06-03 09:37:42 +0200

    add center of calendar skeleton

---
+++ src/main/java/com/chronio/calendar/view/CalendarCenterView.java
@@ -0,0 +1,11 @@
+package com.chronio.calendar.view;
+
+import javafx.scene.layout.VBox;
+
+public final class CalendarCenterView {
+
+    public VBox build() {
+        final VBox box = new VBox(8);
+        return box;
+    }
+}
