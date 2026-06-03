a35c59f0d5b3d1e98dd37dd91aa1b9370de4ac5a
Aurora Loprino <aurora.loprino@studio.unibo.it>
2026-06-03 09:38:52 +0200

    put all the subviews in the calendar view

--- src/main/java/com/chronio/calendar/view/CalendarView.java
+++ src/main/java/com/chronio/calendar/view/CalendarView.java
@@ -1,12 +1,25 @@
 package com.chronio.calendar.view;
 
+import com.chronio.calendar.controller.CalendarController;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
 
 public final class CalendarView {
 
+    private final CalendarController controller;
+    private final Stage stage;
+
+    public CalendarView(final CalendarController controller, final Stage stage) {
+        this.controller = controller;
+        this.stage = stage;
+    }
+
     public Scene build() {
         final BorderPane root = new BorderPane();
+        root.setLeft(new TagSidebarView().build());
+        root.setCenter(new CalendarCenterView().build());
+        root.setRight(new EventSidebarView().build());
         return new Scene(root, 1200, 700);
     }
 }
