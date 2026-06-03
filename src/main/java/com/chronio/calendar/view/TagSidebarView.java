87702f23540c178c6cae25ce6127ef1f7f29b718
Aurora Loprino <aurora.loprino@studio.unibo.it>
2026-06-03 09:35:16 +0200

    add sidebar skeleton

---
+++ src/main/java/com/chronio/calendar/view/TagSidebarView.java
@@ -0,0 +1,12 @@
+package com.chronio.calendar.view;
+
+import javafx.scene.layout.VBox;
+
+public final class TagSidebarView {
+
+    public VBox build() {
+        final VBox box = new VBox(8);
+        box.setPrefWidth(200);
+        return box;
+    }
+}
