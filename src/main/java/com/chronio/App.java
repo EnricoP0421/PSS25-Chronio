e75850c29d4123c4d0a33807e08f609fb0ed523b
Aurora Loprino <aurora.loprino@studio.unibo.it>
2026-06-03 09:40:10 +0200

    put App  with controller and view. Gonna try to open it

--- src/main/java/com/chronio/App.java
+++ src/main/java/com/chronio/App.java
@@ -1,23 +1,44 @@
 package com.chronio;
 
+import com.chronio.budget.controller.BudgetController;
 import com.chronio.budget.model.BudgetService;
 import com.chronio.budget.persistence.JsonBudgetRepository;
-import com.chronio.budget.controller.BudgetController;
 import com.chronio.budget.view.BudgetView;
+import com.chronio.calendar.controller.CalendarControllerImpl;
+import com.chronio.calendar.model.CalendarModelImpl;
+import com.chronio.calendar.persistence.CalendarPersistence;
+import com.chronio.calendar.view.MainView;
+import com.chronio.kanban.controller.BoardControllerImpl;
+import com.chronio.kanban.persistence.BoardPersistence;
+
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-public class App extends Application {
+/**
+ * Punto di ingresso dell'applicazione Chronio.
+ */
+public final class App extends Application {
 
+    /**
+     * Inizializza e mostra la finestra principale.
+     *
+     * @param stage lo stage principale fornito da JavaFX
+     */
     @Override
     public void start(final Stage stage) {
-        final BudgetService service = new BudgetService(new JsonBudgetRepository());
-        final BudgetController controller = new BudgetController(service);
-        final BudgetView budgetView = new BudgetView(controller);
+        final CalendarPersistence calPersistence = new CalendarPersistence(CalendarPersistence.getDefaultPath());
+        final CalendarModelImpl calModel = new CalendarModelImpl(calPersistence.load());
+        final CalendarControllerImpl calController = new CalendarControllerImpl(calModel, calPersistence);
 
-        stage.setTitle("Chronio - Budget");
-        stage.setScene(new Scene(budgetView, 1000, 640));
+        final BoardPersistence boardPersistence = new BoardPersistence(BoardPersistence.getDefaultPath());
+        final BoardControllerImpl boardController = new BoardControllerImpl(boardPersistence.load(), boardPersistence);
+
+        final BudgetController budgetController = new BudgetController(new BudgetService(new JsonBudgetRepository()));
+        final BudgetView budgetView = new BudgetView(budgetController);
+
+        stage.setTitle("Chronio");
+        stage.setScene(new MainView(calController, boardController, budgetView, stage).build());
         stage.show();
     }
 
